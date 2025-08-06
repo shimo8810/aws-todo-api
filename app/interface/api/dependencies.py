80146bb7f826d30f0abcfee27aea84ba2108857e
shimo8810 <shimo.8810@gmail.com>
2025-08-06 22:48:43 +0900

    refactor(dependencies): remove in-memory task list repository and update service dependencies

--- app/interface/api/dependencies.py
+++ app/interface/api/dependencies.py
@@ -1,38 +1,10 @@
 from typing import Annotated
 
 from fastapi import Depends
-from typing_extensions import Self
 
-from ...application.task import TaskService
+from ...application.todo import TodoService
 from ...domain.task_list_repository import TaskListRepository
-
-
-class InMemoryTaskListRepository(TaskListRepository):
-    _instance = None
-
-    def __new__(cls) -> Self:
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-        return cls._instance
-
-    def __init__(self):
-        if not hasattr(self, "tasks"):
-            self.tasks = []
-
-    def store(self, task_list):
-        self.tasks.append(task_list)
-
-    def find_by_id(self, task_list_id):
-        for task in self.tasks:
-            if task.id == task_list_id:
-                return task
-        return None
-
-    def delete(self, task_list_id):
-        self.tasks = [task for task in self.tasks if task.id != task_list_id]
-
-    def list_all(self):
-        return self.tasks
+from ...domain.task_repository import TaskRepository
 
 
 def get_task_list_repository() -> TaskListRepository:
@@ -41,7 +13,22 @@ def get_task_list_repository() -> TaskListRepository:
     )
 
 
-def get_task_service(
-    repo: Annotated[TaskListRepository, Depends(get_task_list_repository)],
-) -> TaskService:
-    return TaskService(task_list_repository=repo)
+def get_task_repository() -> TaskRepository:
+    raise NotImplementedError(
+        "Dependency 'get_task_repository' has not been overridden."
+    )
+
+
+def get_todo_service(
+    task_list_repository: Annotated[
+        TaskListRepository, Depends(get_task_list_repository)
+    ],
+    task_repository: Annotated[
+        TaskRepository,
+        Depends(get_task_repository),
+    ],
+) -> TodoService:
+    return TodoService(
+        task_list_repository=task_list_repository,
+        task_repository=task_repository,
+    )

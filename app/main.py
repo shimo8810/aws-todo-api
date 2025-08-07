7efa9a5497deaeb99d727590da30f66facb63ef3
shimo8810 <shimo.8810@gmail.com>
2025-08-07 21:09:19 +0900

    refactor(main): correct import paths and add task repository dependency

--- app/main.py
+++ app/main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
 from mangum import Mangum
 
-from .infrastructure.db.dydb_task_list_repository import (
+from .infrastructure.db.dynamodb_task_list_repository import (
     get_dynamodb_task_list_repository,
 )
-from .interface.api.dependencies import get_task_list_repository
+from .infrastructure.db.dynamodb_task_repository import (
+    get_dynamodb_task_repository,
+)
+from .interface.api.dependencies import (
+    get_task_list_repository,
+    get_task_repository,
+)
 from .interface.api.router import router
 
 
@@ -13,6 +19,7 @@ def create_app() -> FastAPI:
     app.dependency_overrides[get_task_list_repository] = (
         get_dynamodb_task_list_repository
     )
+    app.dependency_overrides[get_task_repository] = get_dynamodb_task_repository
 
     app.include_router(router)
     return app
